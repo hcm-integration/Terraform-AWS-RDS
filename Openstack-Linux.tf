cbf4ab27c13c8eebfd8b1d394e2cd7f10acd907c
hcm-integration <49394867+hcm-integration@users.noreply.github.com>
2021-08-03 06:50:24 -0400

    Update and rename Openstack-Windows.tf to Openstack-Linux.tf

--- Openstack-Linux.tf
+++ Openstack-Linux.tf
@@ -1,9 +1,9 @@
 # Create Virtual Machine
 
-resource "openstack_compute_instance_v2" "mywinvm" {
+resource "openstack_compute_instance_v2" "mylinvm" {
   count       = var.instancecount
   name        = "${var.name}${count.index + 1}"
-  image_name  = "win12server2"
+  image_name  = "centos7_07"
   flavor_name = lookup(var.flavor, var.size)
   #key_pair        = ""
   security_groups   = ["default"]
