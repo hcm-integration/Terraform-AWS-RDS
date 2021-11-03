d93870bf69b469fc3a5dbab764c01dc500ef49b0
hcm-integration <49394867+hcm-integration@users.noreply.github.com>
2021-11-03 06:51:53 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,9 +1,6 @@
-provider "openstack" {
-  user_name = "TCS_SOLVE"
-  #"svc_se04_hcm"
-  tenant_name = "SE04-SOLVE"
-  password    = "TCS_solve123"
-  # "Cloud1dmin@123"
-  auth_url = "http://172.31.44.141:5000/v2/"
-  region   = "SE4"
+provider "aws" {
+  #  version    = "~> 3.0"
+  region     = "us-west-1"
+  access_key = "AKIA2ICTJLACY6TDKM4G"
+  secret_key = "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"
 }
