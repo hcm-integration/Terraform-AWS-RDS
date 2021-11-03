8289257b10f99b10d311c70222c2e2e1f197299b
hcm-integration <49394867+hcm-integration@users.noreply.github.com>
2021-11-03 06:39:20 -0400

    Update and rename Openstack-Linux.tf to RDS.tf

---
+++ RDS.tf
@@ -0,0 +1,13 @@
+resource "aws_db_instance" "rds" {
+  allocated_storage = var.storage
+  engine            = var.engine
+  engine_version    = var.engver
+  instance_class    = var.dbsize
+  identifier        = var.identifier
+  #identifier_prefix = var.identifier_prefix 
+  name              = var.dbname
+  username          = var.dbusername
+  password          = var.dbpswd
+  #parameter_group_name = "default.mysql5.7"
+  #skip_final_snapshot  = true
+}
